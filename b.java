6c1bd4a7642eab679fea8d11173126324e3525b2
Tamilazharasan <94954901+0tamil@users.noreply.github.com>
2022-11-18 15:15:12 +0530

    2 commit

--- b.java
+++ b.java
@@ -1,62 +1,29 @@
-package f.c.a.e;
+package j.y;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Base64;
-import android.util.Log;
-import java.security.Key;
-import java.security.SecureRandom;
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
+import j.x.d.i;
+import java.util.Random;
 /* loaded from: classes.dex */
-public class b implements c {
-    private Key c;
-    private final SecureRandom b = new SecureRandom();
-    private final Cipher a = Cipher.getInstance("AES/CBC/PKCS7Padding");
+public final class b extends j.y.a {
 
-    public b(Context context) {
-        a aVar = new a(context);
-        SharedPreferences sharedPreferences = context.getSharedPreferences("FlutterSecureKeyStorage", 0);
-        SharedPreferences.Editor edit = sharedPreferences.edit();
-        String string = sharedPreferences.getString("VGhpcyBpcyB0aGUga2V5IGZvciBhIHNlY3VyZSBzdG9yYWdlIEFFUyBLZXkK", null);
-        if (string != null) {
-            try {
-                this.c = aVar.k(Base64.decode(string, 0), "AES");
-                return;
-            } catch (Exception e2) {
-                Log.e("StorageCipher18Impl", "unwrap key failed", e2);
-            }
+    /* renamed from: g  reason: collision with root package name */
+    private final a f3482g = new a();
+
+    /* loaded from: classes.dex */
+    public static final class a extends ThreadLocal<Random> {
+        a() {
         }
-        byte[] bArr = new byte[16];
-        this.b.nextBytes(bArr);
-        SecretKeySpec secretKeySpec = new SecretKeySpec(bArr, "AES");
-        this.c = secretKeySpec;
-        edit.putString("VGhpcyBpcyB0aGUga2V5IGZvciBhIHNlY3VyZSBzdG9yYWdlIEFFUyBLZXkK", Base64.encodeToString(aVar.l(secretKeySpec), 0));
-        edit.apply();
+
+        /* JADX INFO: Access modifiers changed from: protected */
+        /* renamed from: a */
+        public Random initialValue() {
+            return new Random();
+        }
     }
 
-    @Override // f.c.a.e.c
-    public byte[] a(byte[] bArr) {
-        byte[] bArr2 = new byte[16];
-        this.b.nextBytes(bArr2);
-        this.a.init(1, this.c, new IvParameterSpec(bArr2));
-        byte[] doFinal = this.a.doFinal(bArr);
-        byte[] bArr3 = new byte[doFinal.length + 16];
-        System.arraycopy(bArr2, 0, bArr3, 0, 16);
-        System.arraycopy(doFinal, 0, bArr3, 16, doFinal.length);
-        return bArr3;
-    }
-
-    @Override // f.c.a.e.c
-    public byte[] b(byte[] bArr) {
-        byte[] bArr2 = new byte[16];
-        System.arraycopy(bArr, 0, bArr2, 0, 16);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(bArr2);
-        int length = bArr.length - 16;
-        byte[] bArr3 = new byte[length];
-        System.arraycopy(bArr, 16, bArr3, 0, length);
-        this.a.init(2, this.c, ivParameterSpec);
-        return this.a.doFinal(bArr3);
+    @Override // j.y.a
+    public Random c() {
+        Random random = this.f3482g.get();
+        i.c(random, "implStorage.get()");
+        return random;
     }
 }
